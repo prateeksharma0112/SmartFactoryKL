3ca5dac30fac78d6f411a8b1e0e55b76ac06f725
prateek.sharma <prateeksharma0112@gmail.com>
2026-02-11 14:40:44 +0100

    Improve UI

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "../layout/navbar.jsx";
 import Home from "../pages/home/Home";
+import ProductionPlan from "../pages/productionPlan/productionPlan";
 
-export default function App() {
-  return <Home />;
-}
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/production-plan" element={<ProductionPlan />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+
+export default App;
