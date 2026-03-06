db6efee162b82ae20e31419a7ca7c9f37fbae238
prateek.sharma <prateeksharma0112@gmail.com>
2026-03-06 17:23:44 +0100

    update title name

--- frontend/src/layout/navbar.jsx
+++ frontend/src/layout/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">SmartFactory</div>
+      <div className="logo">SmartFactory KL</div>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/production-plan">Production Plan</Link>
